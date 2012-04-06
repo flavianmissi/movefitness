c873f65294b140403567678156f9923b9eaec0f7
Flavia Missi <flaviamissi@gmail.com>
2012-04-06 15:48:17 -0300

    Tests for new field and unicode method

--- content/tests/test_models.py
+++ content/tests/test_models.py
@@ -13,3 +13,10 @@ class OurInstallationModelTestCase(TestCase):
 
     def test_should_have_a_title_field(self):
         self.assertFieldPresence(Content, "title")
+
+    def test_should_have_a_slug_field(self):
+        self.assertFieldPresence(Content, "slug")
+
+    def test_unicode_should_return_the_content_title(self):
+        content = Content(title="Activities")
+        self.assertEqual(unicode(content), u"Activities")

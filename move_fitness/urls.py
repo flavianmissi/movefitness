eb3812fad868a82af3b5644a93a6625188651f0f
Flavia Missi <flaviamissi@gmail.com>
2012-04-06 15:49:22 -0300

    Activating admin and adding url for content renderization

--- move_fitness/urls.py
+++ move_fitness/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from content.views import ContentView
+
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'move_fitness.views.home', name='home'),
-    # url(r'^move_fitness/', include('move_fitness.foo.urls')),
+    url(r'^(?P<slug>[\w-]+)?/$', ContentView.as_view(), name='content'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )

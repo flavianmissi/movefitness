0cd5a96f29c09397bc3c74883d43244091fe8289
Flavia Missi <flaviamissi@gmail.com>
2012-04-07 19:21:31 -0300

    Routing content urls

--- move_fitness/urls.py
+++ move_fitness/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from content.views import ContentView
 
@@ -7,13 +8,8 @@ from content.views import ContentView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'move_fitness.views.home', name='home'),
-    url(r'^(?P<slug>[\w-]+)?/$', ContentView.as_view(), name='content'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<slug>[\w-]+)?/$', ContentView.as_view(), name='content'),
 )
+
+urlpatterns += staticfiles_urlpatterns()

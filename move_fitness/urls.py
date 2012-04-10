dff700f79067881eb4a95af1650a5e95236e4553
Flavia Missi <flaviamissi@gmail.com>
2012-04-10 12:41:17 -0300

    Created our installations url

--- move_fitness/urls.py
+++ move_fitness/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from activities.views import ActivitiesView
+from our_installations.views import OurInstallationsView
 
 
 admin.autodiscover()
@@ -10,6 +11,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'atividades?/$', ActivitiesView.as_view(), name='activities'),
+    url(r'nossas-instalacoes?/$', OurInstallationsView.as_view(), name='our_installations'),
 )
 
 urlpatterns += staticfiles_urlpatterns()

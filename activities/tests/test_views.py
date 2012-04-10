43b8fed8026c0af3f8c6654356f65be07feef58e
Flavia Missi <flaviamissi@gmail.com>
2012-04-09 21:33:11 -0300

    Filling the context of activities view with necessary information

--- activities/tests/test_views.py
+++ activities/tests/test_views.py
@@ -1,13 +1,51 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase, RequestFactory
 
-from activities.views import AcitivitiesView
+from activities.views import ActivitiesView
+from activities.models import Activity
+from content.models import Content
 
 
 class ActivitiesViewsTestCase(TestCase):
 
     def setUp(self):
         self.request = RequestFactory().get('activities')
+        self.content = Content.objects.create(
+            title="Atividades",
+            description="foo bar",
+            slug="atividades"
+        )
+        self.activity = Activity.objects.create(
+            title="do nothing",
+            description="yes, that's a valid activity'"
+        )
+        self.response = ActivitiesView.as_view()(self.request)
+
+    def tearDown(self):
+        if self.content.id:
+            self.content.delete()
+        self.activity.delete()
 
     def test_should_get_acitivities_view_and_be_success(self):
-        response = AcitivitiesView.as_view()(self.request)
+        self.assertEqual(200, self.response.status_code)
+
+    def test_should_get_activities_and_have_the_content_record_referenced_to_it(self):
+        self.assertIsInstance(self.response.context_data["content"], Content)
+
+    def test_should_have_a_list_of_activities_in_the_context(self):
+        self.assertIsInstance(self.response.context_data["activities"][0], Activity)
+
+    def test_should_use_template_named_activities(self):
+        self.assertIn("activities.html", self.response.template_name)
+
+    def test_should_not_get_error_when_theres_no_content_activity_in_the_database(self):
+        self.content.delete()
+        response = ActivitiesView.as_view()(self.request)
+        self.assertEqual(200, response.status_code)
+
+
+class ActivitiesUrlTestCase(TestCase):
+
+    def test_should_request_activities_page_and_be_success(self):
+        response = self.client.get(reverse('activities'))
         self.assertEqual(200, response.status_code)
--- activities/views.py
+++ activities/views.py
@@ -1,9 +1,20 @@
 from django.views.generic import ListView
 
 from activities.models import Activity
+from content.models import Content
 from move_fitness.views import BaseView
 
 
-class AcitivitiesView(ListView, BaseView):
+class ActivitiesView(ListView, BaseView):
 
     model = Activity
+    context_object_name = "activities"
+    template_name = "activities.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = ListView.get_context_data(self, *args, **kwargs)
+        context.update(BaseView.get_context_data(self, *args, **kwargs))
+        activities = Content.objects.filter(slug="atividades")
+        if activities.exists():
+            context["content"] = activities[0]
+        return context

ea12df7ba1b996238607c433db64bf4042e72ab5
Flavia Missi <flaviamissi@gmail.com>
2012-04-15 00:18:00 -0300

    Added slideshow to index

---
+++ move_fitness/static/js/slideshow.js
@@ -0,0 +1,28 @@
+window.addEvent('domready',function() {
+    /* settings */
+    var showDuration = 3000;
+    var container = $('slideshow-container');
+    var imgContainers = container.getElements('.img-element');
+    var currentIndex = 0;
+    var interval;
+
+    /* opacity and fade */
+    imgContainers.each(function(img, i) {
+        if(i > 0) {
+            img.set('opacity',0);
+        }
+    });
+
+    console.log("fooooo");
+    /* worker */
+    var show = function() {
+        imgContainers[currentIndex].fade('out');
+        imgContainers[currentIndex = currentIndex < imgContainers.length - 1 ? currentIndex+1 : 0].fade('in');
+    };
+
+    /* start once the page is finished loading */
+    window.addEvent('load',function() {
+        interval = show.periodical(showDuration);
+    });
+});
+

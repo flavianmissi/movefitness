bcc518149685aedc8170a0ac9df6e98044f25e96
Evandro Flores <eof@eof.com.br>
2012-04-28 00:11:45 -0300

    chng twitter social widget to profile

--- move_fitness/static/js/social.js
+++ move_fitness/static/js/social.js
@@ -9,28 +9,27 @@ window.addEvent('domready', function() {
 });
 
 new TWTR.Widget({
-    version: 2,
-    type: 'profile',
-    rpp: 4,
-    interval: 30000,
-    width: 250,
-    height: 300,
-    
-    theme: {
-        shell: {
-            background: '#8ec1da',
-            color: '#ffffff'
-        },
-        tweets: {
-            background: '#ffffff',
-            color: '#444444',
-            links: '#1985b5'
-        }
-    },
-    features: {
-        scrollbar: false,
-        loop: true,
-        live: true,
-        behavior: 'default'
-    }
+  version: 2,
+  type: 'profile',
+  rpp: 4,
+  interval: 30000,
+  width: 250,
+  height: 300,
+  theme: {
+      shell: {
+          background: '#8ec1da',
+          color: '#ffffff'
+      },
+      tweets: {
+          background: '#ffffff',
+          color: '#444444',
+          links: '#1985b5'
+      }
+  },
+  features: {
+      scrollbar: false,
+      loop: true,
+      live: true,
+      behavior: 'default'
+  }
 }).render().setUser('MoveFitnessRJ').start();

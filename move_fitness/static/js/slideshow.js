914fa5767024195f4f6040f90899fc3d67e18eb7
Flavia Missi <flaviamissi@gmail.com>
2012-05-02 23:05:40 -0300

    Fixed IEca png bug

--- move_fitness/static/js/slideshow.js
+++ move_fitness/static/js/slideshow.js
@@ -23,5 +23,15 @@ window.addEvent('domready', function() {
     window.addEvent('load',function() {
         interval = show.periodical(showDuration);
     });
-});
 
+    var i;
+    for (i in document.images) {
+        if (document.images[i].src) {
+            var imgSrc = document.images[i].src;
+            if (imgSrc.substr(imgSrc.length-4) === '.png' || imgSrc.substr(imgSrc.length-4) === '.PNG') {
+                document.images[i].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled='true',sizingMethod='crop',src='" + imgSrc + "')";
+            }
+        }
+    }
+
+});
